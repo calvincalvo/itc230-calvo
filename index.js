1b17800505b6d6d1ce3a78e88ab36cd59b3800bb
calvincalvo <ccalvo916@gmail.com>
2018-04-05 17:33:15 -0700

    Update index.js
    fixed and now understand routing in this context

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var http = require('http');
 var fs = require("fs");
-http.createServer(function(req, res) {
-    fs.readFile("home.html", function(err, data){
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
+http.createServer(function(req, res)
+    var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('404 Not found');
+      break;
+    }
 }).listen(3000);
