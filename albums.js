fab047922cd0e86b15afaea037f464a91fdfbf77
calvincalvo <ccalvo916@gmail.com>
2018-05-24 09:25:11 -0700

    express yourself

--- albums.js
+++ albums.js
@@ -1,34 +1,35 @@
-// function Person(name, year, artist, genre){
-//     this.name = name;
-//     this.year = year;
-//     this.artist = artist;
-//     this.genre = genre;
-//
-// }
-
 var Albums = [
-    {name: "Daisy",year: "2009", artist: "Brand New", genre:"Alternative Rock"},
-    {name: "Nevermind", year: "1991", artist: "Nirvana", genre:"Grunge"},
-    {name: "Rumours", year: "1977", artist: "Fleetwood Mac", genre:"Pop Rock"},
-    {name: "CTRL", year: "2017", artist: "SZA", genre:"Rhythm Blues"},
-    {name: "Graduation", year: "2007", artist: "Kanye West", genre:"Rap"}
+    {title: "Daisy",year: "2009", artist: "Brand New", genre:"Alternative Rock"},
+    {title: "Nevermind", year: "1991", artist: "Nirvana", genre:"Grunge"},
+    {title: "Rumours", year: "1977", artist: "Fleetwood Mac", genre:"Pop Rock"},
+    {title: "CTRL", year: "2017", artist: "SZA", genre:"Rhythm Blues"},
+    {title: "Graduation", year: "2007", artist: "Kanye West", genre:"Rap"}
 ];
 
-
-exports.getAllAlbums = function(){
+exports.getAllAlbums = () => {
     return Albums;
 };
 
 
-exports.findName  = (name) => {
+exports.findTitle  = (title) => {
         return Albums.find((item) => {
-            return item.name == name;
+            return item.title == title;
 
         });
 };
 
 
-var withoutItem = Albums.slice(1, 4)
-exports.albumDeleted = () => {
-        return withoutItem;
+// var withoutItem = Albums.slice(1, 4)
+// exports.albumDeleted = () => {
+//         return withoutItem;
+// };
+
+exports.delete = (title) => {
+    let oldLength = Albums.length;
+    const originalCount = Albums.length;
+    Albums = Albums.filter((item) =>{
+        return item.title.toLowerCase()!= title;    
+    });
+    //boolean to show item was deleted
+    return {"deleted": Albums.length !== oldLength, "total": Albums.length};
 };
--- index.js
+++ index.js
@@ -1,36 +1,70 @@
-var http = require('http');
-var fs = require("fs");
-var films = require('./albums');
+var http = require('http'); 
+var fs = require('fs');
+var path = require('path');
 
-http.createServer(function(req, res){
-    var path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    case '/getall':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("My favorite albums are " + JSON.stringify(films.getAllAlbums()));
-      res.end();
-      break;
-    case '/get':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("You searched for Daisy " + JSON.stringify(films.findName('Daisy')));
-      res.end();
-      break;
-    case '/delete':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write("You deleted Daisy " + JSON.stringify(films.albumDeleted()));
-      res.end();
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404 Not found');
-      break;
-    }
-}).listen(3000);
+//modules
+var albums = require('./albums');
+const express = require('express');
+var cors = require("cors");
+var bodyParser = require("body-parser");
+let handlebars =  require("express-handlebars");
+
+const app = express();
+app.set('port', process.env.PORT || process.env.IP);
+//middleware
+//does all work for turning/reading json files
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("./public"));
+app.use(cors());
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
+{
+ //remember to use arrow functions
+app.get('/', (req,res) =>{
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+ res.type('text/plain');
+ res.send('About page');
+});
+
+app.get('/getall', (req,res) => {
+  res.json(albums.getAllAlbums());
+});  
+
+app.get('/get', (req,res) => {
+ let result = albums.findTitle(req.query.search_field);
+ res.render('detail', {Title: req.query.search_field, result: result });
+ console.log(req.query);
+});
+
+app.post('/get', (req,res) => {
+ let result = albums.findTitle(req.body.search_field);
+ res.render('detail', {Title: req.body.search_field, result: result });
+});
+
+app.get('/delete', (req, res, next) => {
+ //res.type('text/plain');
+  albums.delete({title: req.query.title}, (err, result) => {
+  let deleted = result;
+  if(err) return next(err);
+  res.type('text/html');
+  res.render('delete', { title: req.query.title, deleted: deleted});
+ });
+});
+
+app.get('*', (req, res) => {
+  res.send('what??? 404 page not found', 404);
+  
+});
+
+}
+
+app.listen(app.get('port'), () => {
+ console.log('Express started');
+});
+
+
