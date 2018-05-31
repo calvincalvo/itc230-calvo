e88a0fcf514e7d062189b1c99392ce9890302fc7
calvincalvo <ccalvo916@gmail.com>
2018-05-31 11:35:36 -0700

    mongodb

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 var path = require('path');
 
 //modules
-var albums = require('./albums');
+var albums = require('./models/albums');
 const express = require('express');
 var cors = require("cors");
 var bodyParser = require("body-parser");
 let handlebars =  require("express-handlebars");
+var mongoose = require('mongoose');
 
 const app = express();
 app.set('port', process.env.PORT || process.env.IP);
@@ -20,48 +21,79 @@ app.use(cors());
 app.engine(".html", handlebars({extname: '.html'}));
 app.set("view engine", ".html");
 
-{
+{//begin express routes
  //remember to use arrow functions
-app.get('/', (req,res) =>{
-    res.render('home');
-});
+   app.get('/', (req,res) =>{
+       res.render('home');
+   });
+   
+   app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('About page');
+   });
+   
+   app.get('/getall', (req,res) => {
+     //res.json(albums.getAllAlbums());
+     // return all records
+     albums.find(function(err, albums){
+      res.send(albums);
+     });
+   });  
+   
+   //find specific item
+   app.get('/get', (req,res,next) => {
+    let result = albums.findOne(req.query.search_field);
+    console.log(req.query);
+     albums.findOne({title: req.query.search_field}, (err, item, next) => {
+       if (err) return next(err);
+       console.log(item);
+       res.render('detail', {title: req.query.search_field, result: result });
+       // other code here
+     });
+   });
+   
+   app.post('/get', (req,res) => {
+    console.log(req.body.search_field);
+    albums.findOne({name: req.body.search_field}, (err, item, next) => {
+       if (err) return next(err);
+       console.log(item);
+       res.render('detail', {title: req.body.search_field, result: item });
+       // other code here
+     });
+   });
+   
+   app.post('/add', (req,res) =>{
+      let newItem = {
+          name: req.body.name, 
+          artist: req.body.artist,
+          year: req.body.year,
+          genre: req.body.genre
+      };
+      console.log(newItem);
+      albums.create(newItem, (err, album) =>{
+       console.log(album);
+          if(err) return (err);
+          return res.write(`${album} succesfully saved`);
+      });
+   });
+   
+   app.post('/delete', (req, res, next) => {
+    let result = albums.deleteOne(req.query.bye);
+    console.log(req.query);
+     albums.deleteOne({name: req.query.bye}, (err, item, next) => {
+       if (err) return next(err);
+       console.log(item);
+       res.send(`${item} has been deleted`);
+       // other code here
+     });
+   });
+   
+   app.get('*', (req, res) => {
+     res.status(404).send('what??? 404 page not found');
+     
+   });
 
-app.get('/about', (req, res) => {
- res.type('text/plain');
- res.send('About page');
-});
-
-app.get('/getall', (req,res) => {
-  res.json(albums.getAllAlbums());
-});  
-
-app.get('/get', (req,res) => {
- let result = albums.findTitle(req.query.search_field);
- res.render('detail', {Title: req.query.search_field, result: result });
- console.log(req.query);
-});
-
-app.post('/get', (req,res) => {
- let result = albums.findTitle(req.body.search_field);
- res.render('detail', {Title: req.body.search_field, result: result });
-});
-
-app.get('/delete', (req, res, next) => {
- //res.type('text/plain');
-  albums.delete({title: req.query.title}, (err, result) => {
-  let deleted = result;
-  if(err) return next(err);
-  res.type('text/html');
-  res.render('delete', { title: req.query.title, deleted: deleted});
- });
-});
-
-app.get('*', (req, res) => {
-  res.send('what??? 404 page not found', 404);
-  
-});
-
-}
+}//end express routes
 
 app.listen(app.get('port'), () => {
  console.log('Express started');
---
+++ models/albums.js
@@ -0,0 +1,22 @@
+//models/films.js
+var mongoose = require('mongoose');
+
+//remote db connection settings. For security, connectionString should be in a separate file not committed to git
+var connectionString = "mongodb://calcal:avatar23@ds217360.mlab.com:17360/schoolio";
+mongoose.connect(connectionString);
+ 
+ var conn = mongoose.connection; 
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+// define Book model in JSON key/value pairs
+// values indicate the data type of each key
+var albumSchema = new mongoose.Schema({
+ name: { type: String, required: true },
+ artist: String,
+ year: String,
+ genre: String
+ 
+}); 
+
+//creates model and makes it avalilable to the app
+module.exports = mongoose.model('albums', albumSchema);
